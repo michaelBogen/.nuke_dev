b5d92d0cd9b062a74fb02156fb49cc181041edbd
Michael Bogen <michaelbogen@me.com>
2020-01-05 00:12:35 -0800

    Create init.py
    first version

---
+++ init.py
@@ -0,0 +1,27 @@
+# ----------------------------------------------------------------------------------------
+# init.py
+# Version: 1.0.1
+# Last Updated: January 4th. 2020
+#-----------------------------------------------------------------------------------------
+
+# -----Define Custom Folder Structure-----------------------------------------------------
+
+
+nuke.pluginAddPath('./gizmos')
+nuke.pluginAddPath('./python')
+nuke.pluginAddPath('./icons')
+
+
+
+#Higx Tools
+nuke.pluginAddPath('./Icons')
+nuke.pluginAddPath('./Tools')
+nuke.pluginAddPath('./Tools/General')
+nuke.pluginAddPath('./Tools/Generators')
+nuke.pluginAddPath('./Tools/Modifiers')
+nuke.pluginAddPath('./Tools/Shaders')
+
+
+nuke.pluginAddPath('./higx/PointRender')
+
+
